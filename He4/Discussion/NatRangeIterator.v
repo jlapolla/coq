c464bb2c2d81cbc0ebe58579163ff73ffe1787bf
Justin LaPolla <j.a.lapolla@gmail.com>
2017-01-23 15:05:01 -0600

    Starting small step approach
    - This approach is based on STLC with references [1], but we're
      extending the STLC language with the names of each class and function.
    
    [1]: http://www.cis.upenn.edu/~bcpierce/sf/current/References.html

--- He4/Discussion/NatRangeIterator.v
+++ He4/Discussion/NatRangeIterator.v
@@ -1,33 +1,56 @@
-Module T_NatRangeIterator.
+Module NatRangeIterator.
 
-Inductive NatRangeIterator : Type :=
-  | C_NatRangeIterator: nat -> nat -> NatRangeIterator.
+Inductive tm : Type :=
+  | tvoid : tm
+  | tnat : nat -> tm
+  | tbool : bool -> tm
+  | tvar : nat -> tm
+  | tassign : tm -> tm -> tm
+  | tref : tm -> tm
+  | tderef : tm -> tm
+  | tloc : nat -> tm
+  | tseq : tm -> tm -> tm
 
-Definition get_first {ST : Type} := ST -> NatRangeIterator -> ST -> nat -> Prop.
+  (* Classes *)
+  | tNatRangeIterator : tm -> tm -> tm -> tm -> tm
 
-Definition set_first {ST : Type} := ST -> NatRangeIterator -> nat -> ST -> Prop.
+  (* Named functions *)
+  | tf1_get_first : tm -> tm
+  | tf2_set_first : tm -> tm -> tm
+  | tf1_get_count : tm -> tm
+  | tf2_set_count : tm -> tm -> tm
+  | tf1_off : tm -> tm
+  | tf1_after : tm -> tm
+  | tf1_forth : tm -> tm
+  | tf1_item : tm -> tm
+  | tf3_test : tm -> tm -> tm -> tm.
 
-Definition get_count {ST : Type} := ST -> NatRangeIterator -> ST -> nat -> Prop.
+Inductive value : tm -> Prop := .
 
-Definition set_count {ST : Type} := ST -> NatRangeIterator -> nat -> ST -> Prop.
+Notation "t ',get_first()'" :=
+  (tf1_get_first t) (at level 0, format "t ',get_first()'") : oo_scope.
 
-Definition off {ST : Type} := ST -> NatRangeIterator -> ST -> bool -> Prop.
+Notation "t1 ',set_first(' t2 ')'" :=
+  (tf2_set_first t1 t2) (at level 0, format "t1 ',set_first(' t2 ')'") : oo_scope.
 
-Definition after {ST : Type} := ST -> NatRangeIterator -> ST -> bool -> Prop.
+Notation "t1 ',test(' t2 , t3 ')'" :=
+  (tf3_test t1 t2 t3) (at level 0, format "t1 ',test(' t2 ,  t3 ')'") : oo_scope.
 
-Definition forth {ST : Type} := ST -> NatRangeIterator -> ST -> Prop.
+Delimit Scope oo_scope with oo.
 
-Definition item {ST : Type} := ST -> NatRangeIterator -> ST -> nat -> Prop.
+Example ex_comma_notation1 :
+  ((tbool false),get_first(),get_first())%oo = tf1_get_first (tf1_get_first (tbool false)).
+Proof. reflexivity. Qed.
 
-Inductive T_NatRangeIterator {ST : Type} : Type :=
-  | C_T_NatRangeIterator: @get_first ST ->
-                          @set_first ST ->
-                          @get_count ST ->
-                          @set_count ST ->
-                          @off ST ->
-                          @after ST ->
-                          @forth ST ->
-                          @item ST ->
-                          T_NatRangeIterator.
+Example ex_comma_notation2 :
+  ((tbool false),set_first(tnat 3),get_first())%oo = tf1_get_first (tf2_set_first (tbool false) (tnat 3)).
+Proof. reflexivity. Qed.
 
-End T_NatRangeIterator.
+Example ex_comma_notation3 :
+  ((tbool false),test(tnat 3, tnat 4))%oo = tf3_test (tbool false) (tnat 3) (tnat 4).
+Proof. reflexivity. Qed.
+
+Notation "t1 ;; t2" :=
+  (tseq t1 t2) (at level 80, right associativity).
+
+End NatRangeIterator.
