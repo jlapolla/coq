9ed87015f32483d4ca7be0d804055c1a349fd464
Justin LaPolla <j.a.lapolla@gmail.com>
2017-01-24 12:07:01 -0600

    Model program state as stack and free store
    - Original idea was to use just a free store as in [1]. However, a stack
      is needed to allow for local variables in functions.
    
    [1]: http://www.cis.upenn.edu/~bcpierce/sf/current/References.html

--- He4/Discussion/ProgramState.v
+++ He4/Discussion/ProgramState.v
@@ -1,17 +1,68 @@
 Module ProgramState.
 
-Require Import T_NatRangeIterator.
+Require Export Coq.Lists.List.
 
-Inductive StateDataType : Type :=
-  | DT_NatRangeIterator : T_NatRangeIterator.NatRangeIterator -> StateDataType.
+Set Implicit Arguments.
 
-Inductive StateTypeType {ST : Type} : Type :=
-  | TT_NatRangeIterator : @T_NatRangeIterator.T_NatRangeIterator ST ->
-                          StateTypeType.
+Section ListOperations.
 
-Inductive ProgramState : Type :=
-  | C_ProgramState: list StateDataType ->
-                    list (@StateTypeType ProgramState) ->
-                    ProgramState.
+Variable A : Type.
+
+Fixpoint replace (n : nat) (a : A) (l : list A) : list A :=
+  match l with
+  | nil => nil
+  | cons a' l' =>
+    match n with
+    | O => cons a l'
+    | S n' => cons a' (replace n' a l')
+    end
+  end.
+
+(** TODO: Fill in Lemmas *)
+
+Fixpoint repeat (x : A) (n : nat) : list A :=
+  match n with
+  | O => nil
+  | S k => cons x (repeat x k)
+  end.
+
+(** TODO: Fill in Lemmas *)
+
+End ListOperations.
+
+Section Stacks.
+
+Variable A : Type.
+
+Definition stack_frame := list A.
+
+Definition stack := list stack_frame.
+
+Definition push := cons.
+
+Definition pop := tl.
+
+Function assign (n : nat) (a : A) (sk : stack) : stack :=
+  match sk with
+  | nil => nil
+  | cons sf' sk' => cons (replace n a sf') sk'
+  end.
+
+(** TODO: Fill in Lemmas *)
+
+End Stacks.
+
+Section Stores.
+
+Variable A : Type.
+
+Definition store := list A.
+
+Definition alloc (a : A) (sr : store) :=
+  app sr (cons a nil).
+
+(** TODO: Fill in Lemmas *)
+
+End Stores.
 
 End ProgramState.
