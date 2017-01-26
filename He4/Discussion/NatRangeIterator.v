70e17f8ec5876d02c96692615827ea23f74efba1
Justin LaPolla <j.a.lapolla@gmail.com>
2017-01-26 07:40:01 -0600

    Add 'tpair' records back to language as 'trc'
    - Stepping lists of terms is a problem, since a list of terms is not
      itself a term, and therefore the 'step' relation is not applicable.
      Using 'trc' makes life easier, even though it introduces the 'is_rc'
      predicate.

--- He4/Discussion/NatRangeIterator.v
+++ He4/Discussion/NatRangeIterator.v
@@ -38,13 +38,16 @@ Inductive tm : Type :=
   (* Control flow *)
   | tseq : tm -> tm -> tm
 
+  (* Records *)
+  | trc : tm -> tm -> tm
+
   (* Named functions *)
   | tf1 : f1 -> tm -> tm
   | tf2 : f2 -> tm -> tm -> tm
 
   (* Classes *)
   | tnew : cl -> tm
-  | tcl : cl -> list tm -> tm.
+  | tcl : cl -> tm -> tm.
 
 Inductive value : tm -> Prop :=
 
@@ -53,7 +56,12 @@ Inductive value : tm -> Prop :=
   | vnat : forall n, value (tnat n)
   | vbool : forall b, value (tbool b)
   | vref : forall n, value (tref n)
-  | vcl : forall c l, Forall value l -> value (tcl c l).
+  | vrc : forall t rc, value t -> value rc -> value (trc t rc)
+
+  (* Classes *)
+  | vcl : forall c t, value t -> value (tcl c t).
+
+Definition tempty := tvoid.
 
 Section Stacks.
 Definition stack := ProgramState.stack tm.
@@ -65,6 +73,143 @@ Section Stores.
 Definition store := ProgramState.store tm.
 Definition sr_read (n : nat) (sr : store) : tm := ProgramState.sr_read n sr tempty.
 End Stores.
+
+Section Records.
+
+(** Records encoded as nested pair terms. *)
+
+Hint Resolve Lt.lt_S_n.
+
+Inductive is_rc : tm -> Prop :=
+  | RC_tempty : is_rc tempty
+  | RC_trc : forall t rc, is_rc rc -> is_rc (trc t rc).
+
+Fixpoint rc_create (n : nat) : tm :=
+  match n with
+  | O => tempty
+  | S n' => trc tempty (rc_create n')
+  end.
+
+Fixpoint rc_length (rc : tm) : nat :=
+  match rc with
+  | trc _ rc' => S (rc_length rc')
+  | _ => O
+  end.
+
+Fixpoint rc_read (n : nat) (rc : tm) : tm :=
+  match n with
+  | O =>
+    match rc with
+    | trc t1 _ => t1
+    | _ => tempty
+    end
+  | S n' =>
+    match rc with
+    | trc _ rc' => rc_read n' rc'
+    | _ => tempty
+    end
+  end.
+
+Fixpoint rc_write (n : nat) (t rc : tm) : tm :=
+  match n with
+  | O =>
+    match rc with
+    | trc _ rc' => trc t rc'
+    | _ => rc
+    end
+  | S n' =>
+    match rc with
+    | trc t1 rc' => trc t1 (rc_write n' t rc')
+    | _ => rc
+    end
+  end.
+
+Lemma rc_create_is_rc:
+  forall n,
+  is_rc (rc_create n).
+Proof with auto.
+  induction n; simpl; constructor...
+  Qed.
+
+Lemma rc_create_length:
+  forall n,
+  rc_length (rc_create n) = n.
+Proof with auto.
+  induction n; simpl...
+  Qed.
+
+Lemma rc_create_correct:
+  forall n m,
+  lt m n ->
+  rc_read m (rc_create n) = tempty.
+Proof with auto.
+  induction n. intros. inversion H.
+  destruct m; simpl...
+  Qed.
+
+Lemma rc_read_overflow:
+  forall rc,
+  is_rc rc ->
+  forall m,
+    le (rc_length rc) m ->
+    rc_read m rc = tempty.
+Proof with auto.
+  intros rc H. induction H; destruct m...
+  simpl. intros. inversion H0.
+  simpl. intros. apply Le.le_S_n in H0...
+  Qed.
+
+Lemma rc_write_is_rc:
+  forall rc,
+  is_rc rc ->
+  forall n t,
+    is_rc (rc_write n t rc).
+Proof with auto.
+  intros rc H.
+  induction H; destruct n; simpl; constructor...
+  Qed.
+
+Lemma rc_write_length:
+  forall rc,
+  is_rc rc ->
+  forall n t,
+    rc_length (rc_write n t rc) = rc_length rc.
+Proof with auto.
+  intros rc H. induction H.
+  destruct n; simpl; constructor...
+  destruct n; simpl...
+  Qed.
+
+Lemma rc_write_correct_1:
+  forall rc,
+  is_rc rc ->
+  forall n m t,
+    lt m (rc_length rc) ->
+    m <> n ->
+    rc_read m (rc_write n t rc) = rc_read m rc.
+Proof with auto.
+  intros rc H. induction H.
+  simpl. intros. inversion H.
+  destruct n.
+  destruct m... intros. exfalso...
+  destruct m...
+  simpl. intros. apply not_eq_n in H1...
+  Qed.
+
+Lemma rc_write_correct_2:
+  forall rc,
+  is_rc rc ->
+  forall n t,
+    lt n (rc_length rc) ->
+    rc_read n (rc_write n t rc) = t.
+Proof with auto.
+  intros rc H. induction H.
+  intros. inversion H.
+  destruct n...
+  simpl. intros...
+  Qed.
+
+End Records.
 
 Reserved Notation "t1 '/' st1 '==>' t2 '/' st2"
   (at level 40, st1 at level 39, t2 at level 39).
