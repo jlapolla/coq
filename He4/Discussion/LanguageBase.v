844f0c774a8cebe970cba540886036ebd7f9e11a
Justin LaPolla <j.a.lapolla@gmail.com>
2017-01-27 15:01:13 -0600

    Add new record construction and field read / write
    - We can construct new objects in the store with 'tnew' (returns a
      reference) or we can construct new objects as values with 'tdefault'
      (returns the object itself). This covers cases for reference classes
      and value classes. 'tnew' and 'trecord' take as parameters the class,
      and the number of fields that the class contains.
    - Field read is straightforward, but field write has a few variants. If
      we're writing to a field for an object in the store (reference class),
      we must update the store. If we're writing to a field for an object on
      the stack (value class), we must update the stack. Writing to a field
      of an object that is a direct value is not allowed, since it has no
      visible effect in the rest of the program.

--- He4/Discussion/LanguageBase.v
+++ He4/Discussion/LanguageBase.v
@@ -29,8 +29,11 @@ Inductive tm : Type :=
   | treturn : tm -> tm
 
   (* Classes *)
-  | tnew : cl -> tm
-  | tcl : cl -> tm -> tm.
+  | tcl : cl -> tm -> tm
+  | tnew : nat -> cl -> tm
+  | tdefault : nat -> cl -> tm
+  | tfield_r : nat -> tm -> tm
+  | tfield_w : nat -> tm -> tm -> tm.
 
 Inductive value : tm -> Prop :=
 
@@ -261,6 +264,58 @@ Inductive step_base : (prod tm (prod stack store)) -> (prod tm (prod stack store
     value v ->
     treturn v / (pair sk sr) ==> v / (pair (pop sk) sr)
 
+  | STcl_r :
+    forall c t0 t0' st st',
+    t0 / st ==> t0' / st' ->
+    tcl c t0 / st ==> tcl c t0' / st'
+
+  | STnew :
+    forall n c0 sk sr,
+    tnew n c0 / pair sk sr ==> tref (length sr) / pair sk (sr_alloc (tcl c0 (rc_create n)) sr)
+
+  | STdefault :
+    forall n c0 st,
+    tdefault n c0 / st ==> tcl c0 (rc_create n) / st
+
+  | STfield_r_r :
+    forall n t0 t0' st st',
+    t0 / st ==> t0' / st' ->
+    tfield_r n t0 / st ==> tfield_r n t0' / st'
+  | STfield_r_ref :
+    forall n n0 c t0 sk sr,
+    sr_read n0 sr = tcl c t0 ->
+    tfield_r n (tref n0) / pair sk sr ==> rc_read n t0 / pair sk sr
+  | STfield_r_cl :
+    forall n c t0 st,
+    value (tcl c t0) ->
+    tfield_r n (tcl c t0) / st ==> rc_read n t0 / st
+
+  | STfield_w_r :
+    forall n t0 t1 t1' st st',
+    (forall n0, t1 <> tvar n0) ->
+    t1 / st ==> t1' / st' ->
+    tfield_w n t0 t1 / st ==> tfield_w n t0 t1' / st'
+  | STfield_w_l :
+    forall n n0 t0 t0' v1 st st',
+    value v1 \/ v1 = tvar n0 ->
+    t0 / st ==> t0' / st' ->
+    tfield_w n t0 v1 / st ==> tfield_w n t0' v1 / st'
+  | STfield_w_var :
+    forall n n0 t0 v0 c sk sr,
+    value v0 ->
+    sk_read_hd n0 sk = tcl c t0 ->
+    tfield_w n v0 (tvar n0) / pair sk sr ==> tvoid / pair (sk_write_hd n0 (tcl c (rc_write n v0 t0)) sk) sr
+  | STfield_w_var_ref :
+    forall n n0 n1 v0 sk sr,
+    value v0 ->
+    sk_read_hd n0 sk = tref n1 ->
+    tfield_w n v0 (tvar n0) / pair sk sr ==> tfield_w n v0 (tref n1) / pair sk sr
+  | STfield_w_ref :
+    forall n n0 t0 v0 c sk sr,
+    value v0 ->
+    sr_read n0 sr = tcl c t0 ->
+    tfield_w n v0 (tref n0) / pair sk sr ==> tvoid / pair sk (sr_write n0 (tcl c (rc_write n v0 t0)) sr)
+
   where "t1 '/' st1 '==>' t2 '/' st2" := (step_base (pair t1 st1) (pair t2 st2)).
 
 Lemma value_does_not_step:
@@ -276,7 +331,34 @@ Proof with auto.
   inversion Hcontra; subst.
   apply IHHval1 in H0. inversion H0.
   apply IHHval2 in H5. inversion H5.
+  intros st t' st' Hcontra.
+  inversion Hcontra; subst.
+  apply IHHval in H0. inversion H0.
   Qed.
+
+Ltac value_step_impossible :=
+  match goal with
+  | H: value ?t, H0: step_base (pair ?t ?st) (pair ?t' ?st') |- _ =>
+    solve [destruct (value_does_not_step t H st t' st'); assumption]
+  end.
+
+Ltac step_base_impossible :=
+  match goal with
+  | H: step_base _ _ |- _ =>
+    solve [inversion H]
+  end.
+
+Ltac step_base_inductive :=
+  match goal with
+  | H: forall z, step_base ?x z -> z = ?y, H0: step_base ?x ?a |- _ = _ =>
+    solve [apply H in H0; inversion H0; auto]
+  end.
+
+Ltac rewrite_invert :=
+  match goal with
+  | H: ?x = ?y, H0: ?x = ?z |- _ = _ =>
+    solve [rewrite H in H0; inversion H0; reflexivity]
+  end.
 
 Lemma step_base_deterministic:
   forall x y,
@@ -287,20 +369,27 @@ Lemma step_base_deterministic:
 Proof with auto.
   intros x y Hxy.
   induction Hxy; intros z Hxz; inversion Hxz; subst;
-  try solve [apply IHHxy in H3; inversion H3; subst; auto];
-  try solve [auto].
-  destruct (value_does_not_step t0 H3 (pair sk sr) t0' st')...
-  destruct (value_does_not_step v0 H (pair sk sr) t0' st')...
-  inversion Hxy.
-  inversion H3.
-  destruct (value_does_not_step t H3 st t' st')...
-  destruct (value_does_not_step v H st t' st'0)...
-  apply IHHxy in H5; inversion H5; subst...
-  destruct (value_does_not_step t0 H3 (pair sk sr) t0' st')...
-  destruct (value_does_not_step v0 H (pair sk sr) t0' st')...
-  apply IHHxy in H2; inversion H2; subst...
-  destruct (value_does_not_step t H2 (pair sk sr) t' st')...
-  destruct (value_does_not_step v H (pair sk sr) t' st')...
+  try solve [value_step_impossible]; (* 36, 36 *)
+  try solve [step_base_inductive]; (* 42, 26 *)
+  try solve [auto]; (* 44, 18 *)
+  try solve [rewrite_invert]; (* 46, 12 *)
+  try solve [step_base_impossible]. (* 46, 6 *)
+  (* STfield_w_r and STfield_w_l *)
+  destruct H5 as [H5 | H5].
+  { value_step_impossible. }
+  { exfalso. apply (H n1). assumption. }
+  (* STfield_w_r and STfield_w_var *)
+  exfalso. apply (H n1). reflexivity.
+  (* STfield_w_r and STfield_w_var_ref *)
+  exfalso. apply (H n1). reflexivity.
+  (* STfield_w_l and STfield_w_r *)
+  destruct H as [H | H].
+  { value_step_impossible. }
+  { exfalso. apply (H5 n0). assumption. }
+  (* STfield_w_var and STfield_w_r *)
+  exfalso. apply (H6 n0). reflexivity.
+  (* STfield_w_var_ref and STfield_w_r *)
+  exfalso. apply (H6 n0). reflexivity.
   Qed.
 
 End LanguageDefinitions.
@@ -316,8 +405,11 @@ Arguments trc {cl} {fn} t t0.
 Arguments tcall {cl} {fn} f t0.
 Arguments texec {cl} {fn} f.
 Arguments treturn {cl} {fn} t.
-Arguments tnew {cl} {fn} c.
 Arguments tcl {cl} {fn} c t0.
+Arguments tnew {cl} {fn} n c0.
+Arguments tdefault {cl} {fn} n c0.
+Arguments tfield_r {cl} {fn} n t0.
+Arguments tfield_w {cl} {fn} n t0 t1.
 
 Module ObjectOrientedNotations.
 
