1e27acdbd658603e5be9a7252988616c19271d41
Justin LaPolla <j.a.lapolla@gmail.com>
2017-02-02 08:10:44 -0600

    Rearrange 'State.v'
    - Refactoring.

--- He4/Language/State.v
+++ He4/Language/State.v
@@ -2,9 +2,18 @@ Require Import He4.Language.Term.
 Require Import Coq.Lists.List.
 Require Import He4.Lists.List.
 
+(** * Types *)
+
 Definition stack_frame : Type := list tm.
 
 Definition stack : Type := list stack_frame.
+
+Definition store : Type := list tm.
+
+Definition state : Type := prod stack store.
+
+(** * Functions *)
+(** ** Stack functions *)
 
 Definition push (sf : stack_frame) (sk : stack) : stack := @cons stack_frame sf sk.
 
@@ -19,11 +28,9 @@ Definition sk_read_hd (n : nat) (sk : stack) : tm :=
 Definition sk_resize_hd (n : nat) (sk : stack) : stack :=
   push (resize n (hd nil sk) tvoid) (pop sk).
 
-Definition empty_stack : stack := push nil nil.
+(** TODO: Lemmas *)
 
-(** TODO: Fill in Lemmas *)
-
-Definition store : Type := list tm.
+(** ** Store functions *)
 
 Definition sr_alloc (a : tm) (sr : store) : store :=
   app sr (cons a nil).
@@ -32,15 +39,11 @@ Definition sr_write (n : nat) (t : tm) (sr : store) : store := @replace tm n t s
 
 Definition sr_read (n : nat) (sr : store) : tm := nth n sr tvoid.
 
-Definition empty_store : store := sr_alloc tvoid nil. (* Position 0 represents the "null" reference *)
+(** TODO: Lemmas *)
 
-(** TODO: Fill in Lemmas *)
+(** ** Function unfolding
 
-Definition state : Type := prod stack store.
-
-Definition empty_state : state := pair empty_stack empty_store.
-
-(** [Arguments] statement with [/] tells tactic [simpl] to unfold these
+    [Arguments] statement with [/] tells tactic [simpl] to unfold these
     functions when arguments before the [/] are provided [[1]].
 
     [[1]] https://coq.inria.fr/distrib/8.4pl4/refman/Reference-Manual010.html##sec395 *)
@@ -53,4 +56,10 @@ Arguments sk_resize_hd n sk /.
 Arguments sr_alloc a sr /.
 Arguments sr_write n t sr /.
 Arguments sr_read n sr /.
+
+(** * Constants *)
+
+Definition empty_stack : stack := push nil nil.
+Definition empty_store : store := sr_alloc tvoid nil. (* Position 0 represents the "null" reference *)
+Definition empty_state : state := pair empty_stack empty_store.
 
