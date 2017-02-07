a6d6a16b0e0a8cbf92e6a9353d252550abafcf28
Justin LaPolla <j.a.lapolla@gmail.com>
2017-02-07 15:02:20 -0600

    'DynamicBinding' lemma skeletons
    - Need to fill these proofs in.

--- He4/Language/DynamicBinding.v
+++ He4/Language/DynamicBinding.v
@@ -24,3 +24,27 @@ Definition called_on_vclassb (c : string) (st : state) : bool :=
   | _ => false
   end.
 
+Lemma called_on_classb_true_iff:
+  forall c st,
+  called_on_classb c st = true <-> called_on_class c st.
+Proof with auto.
+  Abort.
+
+Lemma called_on_classb_false_iff:
+  forall c st,
+  called_on_classb c st = false <-> ~(called_on_class c st).
+Proof with auto.
+  Abort.
+
+Lemma called_on_vclassb_true_iff:
+  forall c st,
+  called_on_vclassb c st = true <-> called_on_vclass c st.
+Proof with auto.
+  Abort.
+
+Lemma called_on_vclassb_false_iff:
+  forall c st,
+  called_on_vclassb c st = false <-> ~(called_on_vclass c st).
+Proof with auto.
+  Abort.
+
