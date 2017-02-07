c159dcb6793863a8dd1b034510f7de7df2f9cd8f
Justin LaPolla <j.a.lapolla@gmail.com>
2017-02-07 13:34:34 -0600

    Prune 'ReduceTactics' dependencies

--- He4/Language/ReduceTactics.v
+++ He4/Language/ReduceTactics.v
@@ -1,10 +1,8 @@
-Require Import He4.Language.Record.
 Require Import He4.Language.State.
 Require Import He4.Language.Step.
 Require Import He4.Language.StepProp.
 Require Import He4.Language.Term.
 Require Import He4.Language.Value.
-Require Import He4.Strings.String.
 
 Ltac reduce_value :=
   match goal with
