9bc0af7630b2422faface35d04e266ee3f091499
Justin LaPolla <j.a.lapolla@gmail.com>
2017-02-07 10:03:01 -0600

    Remove level from 'trc' notation
    - There is no level defined for Coq.Lists.List.ListNotations.

--- He4/Language/Term.v
+++ He4/Language/Term.v
@@ -54,10 +54,10 @@ Inductive tm : Type :=
 
 Module ObjectOrientedNotations.
 
-Notation "'<(' t ')>'" := (trc t tvoid) (at level 20, format "'<(' t ')>'") : oo_scope.
+Notation "'<(' t ')>'" := (trc t tvoid) (format "'<(' t ')>'") : oo_scope.
 
 Notation "'<(' t ',' .. ',' t0 ')>'" :=
-  (trc t .. (trc t0 tvoid) ..) (at level 20, format "'<(' t ','  .. ','  t0 ')>'") : oo_scope.
+  (trc t .. (trc t0 tvoid) ..) (format "'<(' t ','  .. ','  t0 ')>'") : oo_scope.
 
 Notation "t '@' n0" :=
   (tfield_r n0 t) (at level 26, left associativity, format "t  '@'  n0") : oo_scope.
