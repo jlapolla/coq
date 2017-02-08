85bf1cb5b49df7965d38095dc90cf8a613a45995
Justin LaPolla <j.a.lapolla@gmail.com>
2017-02-08 07:58:39 -0600

    Remove 'He4.Language.Core' library

--- He4/Language/Core.v
+++
@@ -1,9 +0,0 @@
-Require Export Coq.Strings.String.
-Require Export He4.Language.Term.
-Require Export Coq.Lists.List.
-Require Export He4.Lists.List.
-Require Export He4.Language.State.
-Require Export He4.Language.Value.
-Require Export He4.Language.Record.
-Require Export He4.Language.Step.
-
