ba2d7352ddfb6695d49ad568c6599c7f1c053f32
Justin LaPolla <j.a.lapolla@gmail.com>
2017-02-08 08:21:05 -0600

    'NatRangeIterator.ReduceTactics' library

---
+++ App/Lib/NatRangeIterator/ReduceTactics.v
@@ -0,0 +1,17 @@
+Require Import He4.Language.DynamicBinding.
+Require Import He4.Language.Term.
+Require Import App.Lib.NatRangeIterator.Step.
+
+Ltac reduce_NatRangeIterator_make :=
+  match goal with
+  | |- step (pair (texec "NatRangeIterator_make") _) _ => eapply STexec_NatRangeIterator_make
+  end.
+
+Ltac reduce_get_at_start :=
+  match goal with
+  | |- step (pair (texec "get_at_start") _) _ =>
+    match eval cbv in (called_on_classb "NatRangeIterator") with
+    | true => eapply STexec_get_at_start
+    end
+  end.
+
