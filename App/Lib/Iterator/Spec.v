3c771f4b2ee4d6bb6d35ca0b6607df34fd2b8211
Justin LaPolla <j.a.lapolla@gmail.com>
2017-02-09 08:26:39 -0600

    'NatRangeIterator.wf' acts on a variable term

--- App/Lib/Iterator/Spec.v
+++ App/Lib/Iterator/Spec.v
@@ -2,11 +2,13 @@ Require Import He4.Language.DynamicBinding.
 Require Import He4.Language.State.
 Require Import He4.Language.StepProp.
 Require Import He4.Language.Term.
+Import ObjectOrientedNotations.
 
 Section Specs.
+Open Scope oo_scope.
 
 Variable step : step_relation.
-Variable wf : state -> Prop.
+Variable wf : tm -> state -> Prop.
 
 Notation "t1 '/' st1 '==>' t2 '/' st2" := (step (pair t1 st1) (pair t2 st2))
   (at level 40, st1 at level 39, t2 at level 39, format "'//' '[' t1 '//' / '//' st1 '//' '==>' '//' t2 '//' / '//' st2 ']'").
@@ -15,13 +17,13 @@ Notation "t1 '/' st1 '==>*' t2 '/' st2" := (multi step (pair t1 st1) (pair t2 st
   (at level 40, st1 at level 39, t2 at level 39, format "'//' '[' t1 '//' / '//' st1 '//' '==>*' '//' t2 '//' / '//' st2 ']'").
 
 Definition off__no_side_effects : Prop :=
-  forall st,
-  wf st ->
+  forall x st,
+  wf x st ->
     exists b st',
-       texec "off" / st ==>* tbool b / st'
-    /\ states_eq_wrt step (texec "off") st st'
-    /\ states_eq_wrt step (texec "after") st st'
-    /\ states_eq_wrt step (texec "item") st st'.
+       (x # "off"|()|) / st ==>* tbool b / st'
+    /\ states_eq_wrt step (x # "off"|()|) st st'
+    /\ states_eq_wrt step (x # "after"|()|) st st'
+    /\ states_eq_wrt step (x # "item"|()|) st st'.
 
 End Specs.
 
--- App/Lib/NatRangeIterator/Spec.v
+++ App/Lib/NatRangeIterator/Spec.v
@@ -2,16 +2,16 @@ Require Import He4.Language.DynamicBinding.
 Require Import He4.Language.State.
 Require Import He4.Language.StepProp.
 Require Import He4.Language.Term.
+Import ObjectOrientedNotations.
 
 Section Specs.
+Open Scope oo_scope.
 
-Variable step : step_relation.
-
-Definition wf (st : state) : Prop :=
-     called_on_class "NatRangeIterator" st
-  /\ (exists b, multi step (pair (texec "get_at_start") st) (pair (tbool b) st))
-  /\ (exists n, multi step (pair (texec "get_count") st) (pair (tnat n) st))
-  /\ (exists n, multi step (pair (texec "get_first") st) (pair (tnat n) st)).
+Definition wf (var : tm) (st : state) : Prop :=
+  exists n ref at_start count first,
+      var = tvar n
+  /\  read_sk_hd n st = tref ref
+  /\  read_sr ref st = tcl "NatRangeIterator" <(tbool at_start, tnat count, tnat first)>.
 
 End Specs.
 
