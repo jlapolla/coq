2a4e733e5b3c0d97322cd494a565fd3988b66217
Justin LaPolla <j.a.lapolla@gmail.com>
2017-02-13 08:01:43 -0600

    Clean up 'Relations.v'

--- He4/Relations/Relations.v
+++ He4/Relations/Relations.v
@@ -5,11 +5,11 @@ Section Definitions.
 Variable A : Type.
 Variable R : relation A.
 
-Definition in_domain (a : A) : Prop :=
-  exists a', R a a'.
+Definition in_domain (x : A) : Prop :=
+  exists y, R x y.
 
-Definition in_range (a : A) : Prop :=
-  exists a', R a' a.
+Definition in_range (y : A) : Prop :=
+  exists x, R x y.
 
 Definition is_function : Prop :=
   forall x y1 y2,
@@ -67,31 +67,6 @@ Proof.
       try solve [assumption].
       apply (H0 x y y0 H) in H5.
       subst y0. assumption.
-  Qed.
-
-Lemma clos_refl_trans_termination_is_function:
-  forall a b1 b2,
-  is_function A R ->
-  ~(in_domain A R b1) ->
-  ~(in_domain A R b2) ->
-  clos_refl_trans A R a b1 ->
-  clos_refl_trans A R a b2 ->
-  b2 = b1.
-Proof.
-  intros.
-  apply clos_rt_rt1n in H2.
-  apply clos_rt_rt1n in H3.
-  generalize dependent H3. generalize dependent H1.
-  generalize dependent H0. generalize dependent H.
-  generalize dependent b2.
-  induction H2.
-  - intros. inversion H3; subst.
-    + reflexivity.
-    + exfalso. apply H0. exists y. assumption.
-  - intros. inversion H4; subst.
-    + exfalso. apply H3. exists y. assumption.
-    + apply (H0 x y y0 H) in H5. subst y0.
-      apply IHclos_refl_trans_1n; try solve [assumption].
   Qed.
 
 End Properties.
