57cea1fca3157bf2ec81abb03e71e5e410a96ab9
Justin LaPolla <j.a.lapolla@gmail.com>
2017-02-15 11:01:04 -0600

    Remove 'var' from 'wf' and 'wf_ex'

--- Software/Doc/Example/Specification/Package/NatRangeIterator.v
+++ Software/Doc/Example/Specification/Package/NatRangeIterator.v
@@ -1,25 +1,23 @@
 Require Import Software.Language.DynamicBinding.
-Require Import Software.Language.State.
 Require Import Software.Language.ExecutionProp.
+Require Import Software.Language.State.
 Require Import Software.Language.Syntax.
 Import ObjectOrientedNotations.
 
 Open Scope oo_scope.
 
 Definition wf_ex (x : term) (st : state) : Prop :=
-  exists var ref at_start count first,
-      x = tvar var
-  /\  read_sk_hd var st = tref ref
+  exists ref at_start count first,
+      x = tref ref
   /\  read_sr ref st = tcl "NatRangeIterator" <(tbool at_start, tnat count, tnat first)>.
 
-Definition wf x var ref st at_start count first : Prop :=
-      x = tvar var
-  /\  read_sk_hd var st = tref ref
+Definition wf x ref st at_start count first : Prop :=
+      x = tref ref
   /\  read_sr ref st = tcl "NatRangeIterator" <(tbool at_start, tnat count, tnat first)>.
 
-Lemma wf_implies_wf_ex:
-  forall x var ref st at_start count first,
-  wf x var ref st at_start count first ->
+Lemma wf_fun_implies_wf:
+  forall x ref st at_start count first,
+  wf x ref st at_start count first ->
   wf_ex x st.
 Proof.
   Abort.
@@ -35,77 +33,77 @@ Notation "t1 '/' st1 '==>*' t2 '/' st2" := (multi exec_step (Cexec_state t1 st1)
   (at level 40, st1 at level 39, t2 at level 39, format "'[' t1 / st1 '==>*' t2 / st2 ']'").
 
 Definition get_at_start : Prop :=
-  forall x var ref st at_start count first,
-  wf x var ref st at_start count first ->
+  forall x ref st at_start count first,
+  wf x ref st at_start count first ->
   (x # "get_at_start"|()|) / st ==>* (tbool at_start) / st.
 
 Definition get_count : Prop :=
-  forall x var ref st at_start count first,
-  wf x var ref st at_start count first ->
+  forall x ref st at_start count first,
+  wf x ref st at_start count first ->
   (x # "get_count"|()|) / st ==>* (tnat count) / st.
 
 Definition get_first : Prop :=
-  forall x var ref st at_start count first,
-  wf x var ref st at_start count first ->
+  forall x ref st at_start count first,
+  wf x ref st at_start count first ->
   (x # "get_first"|()|) / st ==>* (tnat first) / st.
 
 Definition set_at_start : Prop :=
-  forall x var ref st at_start count first,
-  wf x var ref st at_start count first ->
+  forall x ref st at_start count first,
+  wf x ref st at_start count first ->
   forall val,
   (x # "set_at_start"|(val)|) / st ==>* tvoid / write_sr ref (tcl "NatRangeIterator" <(val, tnat count, tnat first)>) st.
 
 Definition set_count : Prop :=
-  forall x var ref st at_start count first,
-  wf x var ref st at_start count first ->
+  forall x ref st at_start count first,
+  wf x ref st at_start count first ->
   forall val,
   (x # "set_count"|(val)|) / st ==>* tvoid / write_sr ref (tcl "NatRangeIterator" <(tbool at_start, val, tnat first)>) st.
 
 Definition set_first : Prop :=
-  forall x var ref st at_start count first,
-  wf x var ref st at_start count first ->
+  forall x ref st at_start count first,
+  wf x ref st at_start count first ->
   forall val,
   (x # "set_first"|(val)|) / st ==>* tvoid / write_sr ref (tcl "NatRangeIterator" <(tbool at_start, tnat count, val)>) st.
 
 Definition off_true : Prop :=
-  forall x var ref st at_start count first,
-  wf x var ref st at_start count first ->
+  forall x ref st at_start count first,
+  wf x ref st at_start count first ->
   count = 0 \/ at_start = true ->
   (x # "off"|()|) / st ==>* (tbool true) / st.
 
 Definition off_false : Prop :=
-  forall x var ref st at_start count first,
-  wf x var ref st at_start count first ->
+  forall x ref st at_start count first,
+  wf x ref st at_start count first ->
   lt 0 count /\ at_start = false ->
   (x # "off"|()|) / st ==>* (tbool false) / st.
 
 Definition after_true : Prop :=
-  forall x var ref st at_start count first,
-  wf x var ref st at_start count first ->
+  forall x ref st at_start count first,
+  wf x ref st at_start count first ->
   count = 0 /\ at_start = false ->
   (x # "after"|()|) / st ==>* (tbool true) / st.
 
 Definition after_false : Prop :=
-  forall x var ref st at_start count first,
-  wf x var ref st at_start count first ->
+  forall x ref st at_start count first,
+  wf x ref st at_start count first ->
   lt 0 count \/ at_start = true ->
   (x # "after"|()|) / st ==>* (tbool true) / st.
 
 Definition forth_at_start : Prop :=
-  forall x var ref st at_start count first,
-  wf x var ref st at_start count first ->
+  forall x ref st at_start count first,
+  wf x ref st at_start count first ->
   at_start = true ->
   (x # "forth"|()|) / st ==>* tvoid / write_sr ref (tcl "NatRangeIterator" <(tbool false, tnat count, tnat first)>) st.
 
 Definition forth : Prop :=
-  forall x var ref st at_start count first,
-  wf x var ref st at_start count first ->
+  forall x ref st at_start count first,
+  wf x ref st at_start count first ->
   at_start = false ->
   (x # "forth"|()|) / st ==>* tvoid / write_sr ref (tcl "NatRangeIterator" <(tbool at_start, tnat (count - 1), tnat (first + 1))>) st.
 
 Definition item : Prop :=
-  forall x var ref st at_start count first,
-  wf x var ref st at_start count first ->
+  forall x ref st at_start count first,
+  wf x ref st at_start count first ->
   (x # "item"|()|) / st ==>* (tnat first) / st.
 
 End Specs.
