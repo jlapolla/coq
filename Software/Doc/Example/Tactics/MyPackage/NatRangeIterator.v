74e83b4c28f29c0556dc79df96aa6cbcc8659c67
Justin LaPolla <j.a.lapolla@gmail.com>
2017-02-15 08:40:28 -0600

    Move 'NatRangeIterator.Execution' library

--- Software/Doc/Example/Tactics/MyPackage/NatRangeIterator.v
+++ Software/Doc/Example/Tactics/MyPackage/NatRangeIterator.v
@@ -1,7 +1,7 @@
 Require Import Software.Language.DynamicBinding.
 Require Import Software.Language.State.
 Require Import Software.Language.Syntax.
-Require Import App.Lib.NatRangeIterator.Execution.
+Require Import Software.Doc.Example.Implementation.MyPackage.NatRangeIterator.
 
 Ltac reduce_function class fn rule :=
   match goal with
